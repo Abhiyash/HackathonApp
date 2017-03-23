79433188aa31afe54977d56761741e1368b03561
Abhiyash <abhiyashm@gmail.com>
2017-03-23 09:34:44 +0530

    I have added database connectivity. Now the app is fetching data from cloud. Worker Login is now available.

--- app/src/main/java/com/example/abhiyash/hackathonapp/WL1.java
+++ app/src/main/java/com/example/abhiyash/hackathonapp/WL1.java
@@ -13,10 +13,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
 
 public class WL1 extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+        EditText e1,e2;
+        Button b1;
+    String url="http://10.0.2.2:48278/WebApplication1/NewWebService?WSDL";
+    String ns="http://abc/";
+    String mname="workerlogin";
+    String SOAP_ACTION="http://abc/workerlogin";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +46,11 @@ public class WL1 extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        e1=(EditText)findViewById(R.id.editText2);
+        e2=(EditText)findViewById(R.id.editText3);
+        b1=(Button)findViewById(R.id.button5);
+        b1.setOnClickListener(this);
     }
 
     @Override
@@ -90,5 +109,44 @@ public class WL1 extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        String s1="",s2="";
+        s1=e1.getText().toString();
+        s2=e2.getText().toString();
+        try{
+            SoapObject msg=new SoapObject(ns,mname);
+            msg.addProperty("Worker_id",s1);
+            msg.addProperty("Password",s2);
+            SoapSerializationEnvelope ev=new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            ev.setOutputSoapObject(msg);
+            HttpTransportSE ttp=new HttpTransportSE(url);
+            try{
+                ttp.call(SOAP_ACTION,ev);
+                SoapPrimitive sp=(SoapPrimitive)ev.getResponse();
+                String s3="";
+                s3+=sp.toString();
+                if(s3.equals("false"))
+                {
+                    Toast.makeText(WL1.this, "Invalid ID or Password", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    //Intent it=new Intent(WL1.this,Tb2.class);
+                    //startActivity(it);
+                    Toast.makeText(WL1.this, "Correct credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+            catch (Exception e)
+            {
+                Toast.makeText(WL1.this, ""+e, Toast.LENGTH_SHORT).show();
+            }
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(WL1.this, ""+e, Toast.LENGTH_SHORT).show();
+        }
     }
 }
